4096b8a6f1b83ce516fde0b24962d5ba9e1dde55
Minjae Kim <minjae.kim@mytrum.com>
2022-07-29 17:31:31 +0900

    feat: Bit 연산 - SumCase 구현

--- src/test/java/training/bit/SumCaseTest.java
+++ src/test/java/training/bit/SumCaseTest.java
@@ -11,13 +11,14 @@ public class SumCaseTest {
     @Test
     void 성공() {
         //given:
-        int[] source = {1,2,3,4,5,6,7,8,9}; //1~9 숫자 집합의
+        int[] source = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; //1~9 숫자 집합의
         int bitCount = 2; //두 수의 합이
         int expectSum = 8; // 합이 8인 것
         //when:
         SumCase sumCase = new SumCase(source, bitCount, expectSum);
         sumCase.expect();
         List<int[]> resultList = sumCase.getResults();
+        sumCase.printResult();
         int resultCount = sumCase.getResultCount();
         //then:
         assertThat(resultCount).isEqualTo(3);
